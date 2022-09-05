0c15faa11e586d2b9287bb607858b6e9b3c9b897
lorehaze <lorenzo.baldari.98@gmail.com>
2022-09-05 20:13:28 +0200

    Uploader partially working

--- src/main/java/com/swa/files/ProfileUpload.java
+++ src/main/java/com/swa/files/ProfileUpload.java
@@ -5,13 +5,17 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import com.swa.dbconnection.Database;
+import com.swa.user.UserDao;
+import com.swa.user.UserBean;
 
 /**
  * Servlet implementation class PetServlet
@@ -35,33 +39,50 @@ public class ProfileUpload extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		
-		
-		InputStream inputStream = null;// input stream of uploaded file
-		Part part = request.getPart("photo");
-		if (part != null) {
-			System.out.println(part.getName());
-			System.out.println(part.getSize());
-			System.out.println(part.getContentType());
-			inputStream = part.getInputStream();
-		}
-		// Now Create a connection and send it to DB...
-		Connection conn = Database.getConn_update();
-		// String sql = "INSERT INTO user (profile_photo) VALUES (?) WHERE user_id = 1";
-		String sql = "UPDATE user SET profile_photo = ? WHERE user_id = 1";
-		try {
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setBlob(1, inputStream);
-			int i = ps.executeUpdate();
-			if (i > 0) {
-				request.setAttribute("success", "Picture Added Successfully");
-				request.getRequestDispatcher("index.jsp").forward(request, response);
+		response.setContentType("text/html");
+		Cookie ck[] = request.getCookies();
+		String email = null;
+
+		if (ck != null) {
+			email = ck[1].getValue();
+			if (email.contains("@")) {
+
+				UserBean user = new UserBean();
+
+				user.setEmail(email);
+
+				UserDao dao = new UserDao();
+
+				int user_id = dao.getID(user);
+
+				InputStream inputStream = null;// input stream of uploaded file
+				Part part = request.getPart("photo");
+				if (part != null) {
+					System.out.println(part.getName());
+					System.out.println(part.getSize());
+					System.out.println(part.getContentType());
+					inputStream = part.getInputStream();
+				}
+				// Now Create a connection and send it to DB...
+				Connection conn = Database.getConn_update();
+				// String sql = "INSERT INTO user (profile_photo) VALUES (?) WHERE user_id = 1";
+				String sql = "UPDATE user SET profile_photo = ? WHERE user_id = ?";
+				try {
+					PreparedStatement ps = conn.prepareStatement(sql);
+					ps.setBlob(1, inputStream);
+					ps.setInt(2, user_id);
+					int i = ps.executeUpdate();
+					if (i > 0) {
+						request.setAttribute("success", "Picture Added Successfully");
+						request.getRequestDispatcher("index.jsp").forward(request, response);
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				response.sendRedirect("login.jsp");
 			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-
 	}
-
 }
--- src/main/java/com/swa/user/ProfileServlet.java
+++ src/main/java/com/swa/user/ProfileServlet.java
@@ -29,20 +29,27 @@ public class ProfileServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		/*
+		 * response.setContentType("text/html");
+		 * 
+		 * Cookie ck[] = request.getCookies();
+		 * 
+		 * if (Arrays.stream(ck).anyMatch("email"::equals)) { String email =
+		 * ck[1].getValue(); if (email.contains("@")) {
+		 * response.sendRedirect("profile.jsp"); } else {
+		 * response.sendRedirect("login.jsp"); } } else {
+		 * response.sendRedirect("login.jsp"); }
+		 */
 		response.setContentType("text/html");
-
 		Cookie ck[] = request.getCookies();
-
-		if (Arrays.stream(ck).anyMatch("email"::equals)) {
-			String email = ck[1].getValue();
+		String email=null;
+		if (ck != null) {
+			email = ck[1].getValue();
 			if (email.contains("@")) {
 				response.sendRedirect("profile.jsp");
 			} else {
 				response.sendRedirect("login.jsp");
 			}
-		} else {
-			response.sendRedirect("login.jsp");
 		}
-
 	}
 }
